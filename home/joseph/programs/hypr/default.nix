a1c57e5b99403ead4afa4675e27a82f4b8dabf86
joseph <joseph.surin@gmail.com>
2023-11-26 00:32:58 +1100

    screenshot, hyprland nix config, ssh

--- home/joseph/programs/hypr/default.nix
+++ home/joseph/programs/hypr/default.nix
@@ -1,25 +1,152 @@
 { config, pkgs, inputs, ... }:
 
+let
+  ss_wrap = ss_type:
+    "[ -d $SCREENSHOT_DIR/`date +\"%Y-%m\"` ]" +
+    " || mkdir $SCREENSHOT_DIR/`date +\"%Y-%m\"`" +
+    " && grimblast --cursor --freeze copysave ${ss_type}" +
+    " $SCREENSHOT_DIR/`date +\"%Y-%m\"`/`date +\"%Y-%m-%d-at-%I-%M-%S%p-${ss_type}.png\"`";
+in
 {
   wayland.windowManager.hyprland = {
-    enable = false;
-    #enable = true;
-    extraConfig = builtins.readFile ./hyprland.conf;
-  };
+    enable = true;
+    enableNvidiaPatches = true;
+    settings = {
+      monitor = [
+        "DVI-D-1,2560x1080,0x0,1"
+        "VGA-1,1920x1080,2560x0,1"
+      ];
 
-  # temporary
-  home.file.".config/hypr/hyprland.conf".source = ./hyprland.conf;
+      exec-once = "hyprpaper & lxqt-policykit-agent & waybar";
+
+      env = "XCURSOR_SIZE,24";
+
+      input = {
+        kb_layout = "us";
+        follow_mouse = "1";
+        touchpad.natural_scroll = "no";
+        sensitivity = 0;
+      };
+
+      general = {
+        gaps_in = 0;
+        gaps_out = 0;
+        border_size = 1;
+        "col.active_border" = "rgba(33ccffee)";
+        "col.inactive_border" = "rgba(595959aa)";
+        layout = "dwindle";
+      };
+
+      decoration = {
+        blur.enabled = false;
+        rounding = 0;
+        drop_shadow = "no";
+      };
+
+      animations.enabled = "no";
+
+      dwindle = {
+        pseudotile = "yes";
+        preserve_split = "yes";
+      };
+
+      master = {
+        new_is_master = false;
+        orientation = "right";
+      };
+
+      gestures.workspace_swipe = "off";
+
+      misc = {
+        disable_hyprland_logo = true;
+        disable_splash_rendering = true;
+      };
+
+      binds = {
+        workspace_back_and_forth = true;
+        allow_workspace_cycles = true;
+      };
+
+      "$mainMod" = "SUPER";
+      bind = [
+        "$mainMod, RETURN, exec, alacritty"
+        "$mainMod SHIFT, Q, killactive,"
+        "$mainMod SHIFT, E, exit,"
+        "$mainMod SHIFT, R, exec, hyprctl reload,"
+        "$mainMod SHIFT, R, forcerendererreload,"
+        "$mainMod SHIFT, SPACE, togglefloating,"
+        "$mainMod, D, exec, rofi -show drun"
+        "$mainMod, T, togglesplit,"
+        "$mainMod, F, fullscreen, 0"
+        "$mainMod, TAB, workspace, previous"
+
+        # dunst
+        "CTRL, GRAVE, exec, dunstctl history-pop"
+        "CTRL, SPACE, exec, dunstctl close"
+        "CTRL SHIFT, SPACE, exec, dunstctl close-all"
+
+        # grimblast
+        "CTRL SHIFT, 4, exec, ${ss_wrap "area"}"
+        "CTRL SHIFT, 3, exec, ${ss_wrap "active"}"
+        "CTRL SHIFT, 2, exec, ${ss_wrap "output"}"
+        "CTRL SHIFT, 1, exec, ${ss_wrap "screen"}"
+        ", mouse:275, exec, grimblast --cursor --freeze copy area"
+        ", mouse:276, exec, wlrctl keyboard type v modifiers CTRL"
+
+        "$mainMod, H, movefocus, l"
+        "$mainMod, L, movefocus, r"
+        "$mainMod, J, movefocus, d"
+        "$mainMod, K, movefocus, u"
+        "$mainMod SHIFT, H, movewindow, l"
+        "$mainMod SHIFT, L, movewindow, r"
+        "$mainMod SHIFT, J, movewindow, d"
+        "$mainMod SHIFT, K, movewindow, u"
+
+        "$mainMod, 1, workspace, 1"
+        "$mainMod, 2, workspace, 2"
+        "$mainMod, 3, workspace, 3"
+        "$mainMod, 4, workspace, 4"
+        "$mainMod, 5, workspace, 5"
+        "$mainMod, 6, workspace, 6"
+        "$mainMod, 7, workspace, 7"
+        "$mainMod, 8, workspace, 8"
+        "$mainMod, 9, workspace, 9"
+        "$mainMod, 0, workspace, 10"
+
+        "$mainMod SHIFT, 1, movetoworkspacesilent, 1"
+        "$mainMod SHIFT, 2, movetoworkspacesilent, 2"
+        "$mainMod SHIFT, 3, movetoworkspacesilent, 3"
+        "$mainMod SHIFT, 4, movetoworkspacesilent, 4"
+        "$mainMod SHIFT, 5, movetoworkspacesilent, 5"
+        "$mainMod SHIFT, 6, movetoworkspacesilent, 6"
+        "$mainMod SHIFT, 7, movetoworkspacesilent, 7"
+        "$mainMod SHIFT, 8, movetoworkspacesilent, 8"
+        "$mainMod SHIFT, 9, movetoworkspacesilent, 9"
+        "$mainMod SHIFT, 0, movetoworkspacesilent, 10"
+      ];
+      bindm = [
+        "$mainMod, mouse:272, movewindow"
+      ];
+    };
+    extraConfig = ''
+      bind = $mainMod, R, submap, resize
+      submap = resize
+      binde = , H, resizeactive, -50 0
+      binde = , L, resizeactive, 50 0
+      binde = , K, resizeactive, 0 -50
+      binde = , J, resizeactive, 0 50
+      binde = SHIFT, H, resizeactive, -10 0
+      binde = SHIFT, L, resizeactive, 10 0
+      binde = SHIFT, K, resizeactive, 0 -10
+      binde = SHIFT, J, resizeactive, 0 10
+      bind = , ESCAPE, submap, reset
+      submap = reset
+    '';
+  };
 
   home.packages = with pkgs; [
     hyprpaper
   ];
   home.file.".config/hypr/hyprpaper.conf".source = ./hyprpaper.conf;
   home.file.".config/hypr/wallpaper.png".source = ./wallpaper.png;
-
-  home.sessionVariables = {
-    WLR_NO_HARDWARE_CURSORS = "1";
-    XDG_CURRENT_DESKTOP = "Hyprland";
-    XDG_SESSION_DESKTOP = "Hyprland";
-    XDG_SESSION_TYPE = "wayland";
-  };
 }
--- hosts/ice/default.nix
+++ hosts/ice/default.nix
@@ -42,12 +42,17 @@
 
   environment.sessionVariables = {
     WLR_NO_HARDWARE_CURSORS = "1";
+    SCREENSHOT_DIR = "/media/winarch-shared/screenshots/";
   };
 
   users.users.joseph = {
     isNormalUser = true;
-    extraGroups = [ "wheel" "networkmanager" "docker" ];
+    extraGroups = [ "wheel" "networkmanager" "docker" "uinput" ];
     packages = with pkgs; [
+    ];
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGNlFlNSbKZpHABOYwbj5peuGvK7c6ywZ+WE7RA/sBeY joseph@eeeeee"
+      "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCkTGykM7sMbxx5MKovPws2OpKYsQuFHq0OBNTiTCOb3ulOMcfnOQfrp6/GCl/X7OajP2FvvwnH4YjD3sCNlcbmMdNSSUhHAI4lMwDcPkq2tBmTqoAW8W+zTOkFhziJijwbmRvlrrl+VL3C5fceTTOyKA97MjOCBoVBhsF89n4KP6Y6MQC2KoqotkhJI8pVENJMS095mNXtEhvIe2hU5BEYHLTXZ4o6R72iRoLc3VU1/VznT8FrnSE3VO578u/QuBT9dvVyySTe3dTIBEondBVoZV2Tj2r0XCvwxpvio9Yb1Q9bl5ugcv7LpuzzLSzq70cySYYDuDZYgJIPOvb2TqzFcEaIuHV35eKmqBvYN3BBkU444sR70ANtt8kz8tpqJo3Ou4kLn19AAFYXs7PN6soUgCDvm0R1Zz4tPf5beAeQW0iAKSm0zNNcqTjkwNz8G5LmIvSawXlP3PfFQzkSlYJfRxVV/KR4dg9/fDQeFQoBCxHo63EBUw4SivgUtK1Fby0= u0_a341@localhost"
     ];
   };
 
@@ -62,38 +67,45 @@
   };
   environment.systemPackages = with pkgs; pkgs.lib.lists.flatten [
     home-manager
-    vim
-    neovim
+    firefox
     bitwarden
     discord
     telegram-desktop
+    chromium
+
     wget
     curl
-    firefox
-    chromium
     file
-    swayimg
-    imv
+    ripgrep
+    ripgrep-all
+    fd
+    bat
+    eza
+    jq
+    wev
+    wlrctl
+    httpie
+    aria
+    dig
+    dogdns
+
+    vim
+    neovim
     git
     zsh
     kitty
     alacritty
     foot
-    hyprpaper
+
+    swayimg
+    imv
     imagemagick
-    ripgrep
-    ripgrep-all
+
+    hyprpaper
     wl-clipboard
     wlr-randr
-    fd
-    bat
-    eza
-    jq
-    httpie
-    aria
-    dig
-    dogdns
     libnotify
+
     (let
       python-pkgs = ps: with ps; [
         pycryptodome
@@ -116,6 +128,14 @@
   programs.hyprland.enable = true;
   programs.hyprland.xwayland.enable = true;
   programs.ssh.startAgent = true;
+  services.tailscale.enable = true;
+  services.openssh = {
+    enable = true;
+    settings = {
+      PasswordAuthentication = false;
+      PermitRootLogin = "no";
+    };
+  };
 
   fonts.packages = with pkgs; [
     powerline-fonts
